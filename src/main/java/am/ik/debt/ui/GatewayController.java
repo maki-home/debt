87816a2181defa762c8f8c39631695f1dd74ddeb
making <makingx@gmail.com>
2017-03-05 17:09:10 +0900

    Support Zipkin headers

--- src/main/java/am/ik/debt/ui/GatewayController.java
+++ src/main/java/am/ik/debt/ui/GatewayController.java
@@ -1,13 +1,17 @@
 package am.ik.debt.ui;
 
 import static org.springframework.web.reactive.function.BodyInserters.fromPublisher;
+import static org.springframework.web.reactive.function.client.ClientRequest.from;
+import static org.springframework.web.reactive.function.client.ExchangeFilterFunction.ofRequestProcessor;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -29,15 +33,24 @@ public class GatewayController {
 		this.props = props;
 	}
 
+	ExchangeFilterFunction gatewayFilter(UserPrincipal principal, HttpHeaders headers) {
+		return ofRequestProcessor((request) -> Mono.just(from(request)
+				.header(HttpHeaders.AUTHORIZATION, "Bearer " + principal.getToken())
+				.header("X-B3-SpanId", headers.getFirst("X-B3-Spanid"))
+				.header("X-B3-TraceId", headers.getFirst("X-B3-Traceid")).build()));
+	}
+
 	@GetMapping("gateway")
-	Mono<Map<String, Object>> gateway(UserPrincipal principal) {
-		Mono<JsonNode> debts = this.webClient.get().uri(props.getDebtUrl() + "/v1/debts")
-				.header(HttpHeaders.AUTHORIZATION, "Bearer " + principal.getToken())
-				.exchange().then(x -> x.bodyToMono(JsonNode.class));
-		Mono<JsonNode> members = this.webClient.get()
-				.uri(props.getAuthorizationUrl() + "/v1/members")
-				.header(HttpHeaders.AUTHORIZATION, "Bearer " + principal.getToken())
-				.exchange().then(x -> x.bodyToMono(JsonNode.class));
+	Mono<Map<String, Object>> gateway(UserPrincipal principal,
+			ServerWebExchange exchange) {
+		Mono<JsonNode> debts = this.webClient
+				.filter(gatewayFilter(principal, exchange.getRequest().getHeaders()))
+				.get().uri(props.getDebtUrl() + "/v1/debts").exchange()
+				.then(x -> x.bodyToMono(JsonNode.class));
+		Mono<JsonNode> members = this.webClient
+				.filter(gatewayFilter(principal, exchange.getRequest().getHeaders()))
+				.get().uri(props.getAuthorizationUrl() + "/v1/members").exchange()
+				.then(x -> x.bodyToMono(JsonNode.class));
 		return Mono.when(debts, members).map(t -> {
 			Map<String, Object> response = new LinkedHashMap<>();
 			response.put("me", principal.getUaaUser());
@@ -48,19 +61,22 @@ public class GatewayController {
 	}
 
 	@PostMapping("debts")
-	Mono<JsonNode> postDebts(@RequestBody Mono<JsonNode> body, UserPrincipal principal) {
-		return this.webClient.post().uri(props.getDebtUrl() + "/v1/debts")
-				.header(HttpHeaders.AUTHORIZATION, "Bearer " + principal.getToken())
+	Mono<JsonNode> postDebts(@RequestBody Mono<JsonNode> body, UserPrincipal principal,
+			ServerWebExchange exchange) {
+		return this.webClient
+				.filter(gatewayFilter(principal, exchange.getRequest().getHeaders()))
+				.post().uri(props.getDebtUrl() + "/v1/debts")
 				.exchange(fromPublisher(body, JsonNode.class))
 				.then(x -> x.bodyToMono(JsonNode.class));
 	}
 
 	@PostMapping("debts/{debtId}/repayments")
 	Mono<JsonNode> postRepayments(@RequestBody Mono<JsonNode> body,
-			@PathVariable String debtId, UserPrincipal principal) {
-		return this.webClient.post()
-				.uri(props.getDebtUrl() + "/v1/debts/{debtId}/repayments", debtId)
-				.header(HttpHeaders.AUTHORIZATION, "Bearer " + principal.getToken())
+			@PathVariable String debtId, UserPrincipal principal,
+			ServerWebExchange exchange) {
+		return this.webClient
+				.filter(gatewayFilter(principal, exchange.getRequest().getHeaders()))
+				.post().uri(props.getDebtUrl() + "/v1/debts/{debtId}/repayments", debtId)
 				.exchange(fromPublisher(body, JsonNode.class))
 				.then(x -> x.bodyToMono(JsonNode.class));
 	}
