069a46c6c6d0ca60f7d34ad80191c0bb171f56d1
making <makingx@gmail.com>
2017-03-05 16:24:26 +0900

    Use basicAuthentication filter

--- src/main/java/am/ik/debt/AppConfig.java
+++ src/main/java/am/ik/debt/AppConfig.java
@@ -1,5 +1,7 @@
 package am.ik.debt;
 
+import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
+
 import java.util.Map;
 
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
@@ -9,7 +11,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.crypto.codec.Base64;
 import org.springframework.security.jwt.crypto.sign.RsaVerifier;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -26,9 +27,8 @@ public class AppConfig {
 	RsaVerifier rsaVerifier(WebClient webClient, ResourceServerProperties resource) {
 		String username = resource.getClientId();
 		String password = resource.getClientSecret();
-		byte[] token = Base64.encode((username + ":" + password).getBytes());
-		return webClient.get().uri(resource.getJwt().getKeyUri())
-				.header("Authorization", "Basic " + new String(token)).exchange()
+		return webClient.filter(basicAuthentication(username, password)).get()
+				.uri(resource.getJwt().getKeyUri()).exchange()
 				.then(x -> x.bodyToMono(Map.class))
 				.map(key -> new RsaVerifier(key.get("value").toString())).block();
 	}
