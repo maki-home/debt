969d09adc59d3b3b47186ecec213c66a7cbbdc59
making <makingx@gmail.com>
2017-04-22 02:40:07 +0900

    wait redis

--- ci/tasks/unit-test-with-redis.sh
+++ ci/tasks/unit-test-with-redis.sh
@@ -9,6 +9,7 @@ docker tag "$(cat redis/image-id)" "$(cat redis/repository):$(cat redis/tag)"
 docker images
 
 docker-compose -f repo/docker-compose.yml up -d
+sh utils/scripts/wait-until-port-available.sh localhost 6379
 sh utils/scripts/add-repos-in-pom-xml.sh repo && \
 sh utils/scripts/mvn.sh test repo
 docker-compose -f repo/docker-compose.yml down
